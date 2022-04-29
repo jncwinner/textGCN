f07dd55c719f5ded6264e623239ef6890bcd20fa
jncwinner <43044912+jncwinner@users.noreply.github.com>
2022-04-29 14:26:34 -0600

    Add files via upload

---
+++ SubGNN/config.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+
+# directory where data and results will be stored
+# TODO: *UPDATE TO YOUR DIRECTORY OF CHOICE*
+PROJECT_ROOT = Path('C:/Users/jncwi/OneDrive/School/CS637/SubGNN')
+GENERATE_SYNTHETIC_G = False
+GENERATE_NODE_EMB = True
+DATASET_DIR = "C:/Users/jncwi/OneDrive/School/CS637/SubGNN/yahoo"
+
+# padding
+PAD_VALUE = 0
+
